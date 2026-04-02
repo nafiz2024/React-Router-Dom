7eb6a6ad47a2122268edcdc09bfcab197c6c6294
Nafiz Alam <nafizalam.dev@gmail.com>
2026-04-03 03:23:35 +0600

    Practice On Going

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,35 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import Root from './components/Root/Root.jsx';
+import Home from './components/Home/Home.jsx';
+import Mobiles from './components/Mobiles/Mobiles.jsx';
+
+const router = createBrowserRouter ([
+  {
+    path: '/',
+    Component: Root,
+    children: [
+      {index: true, Component: Home},
+      {path: 'mobiles', Component: Mobiles}
+    ]
+  },
+
+  {
+    path: 'about',
+    element: <div className="">About me here</div>
+  },
+
+  {
+    path: 'app',
+    Component: App
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </StrictMode>,
 )
