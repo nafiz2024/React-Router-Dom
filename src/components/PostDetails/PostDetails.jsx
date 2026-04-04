dc45f1c44a793bf022a20266be495062a5f04671
Nafiz Alam <nafizalam.dev@gmail.com>
2026-04-04 23:53:45 +0600

    Practice On Going

--- src/components/PostDetails/PostDetails.jsx
+++ src/components/PostDetails/PostDetails.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
-import { useLoaderData } from 'react-router';
+import { useLoaderData, useNavigate } from 'react-router';
 
 const PostDetails = () => {
 
     const post = useLoaderData();
 
+    const navigate = useNavigate();
+
+    const handleGoBack = () => {
+        navigate(-1);
+    }
+
     return (
         <div className='border-2 border-green-300 p-5 rounded-2xl my-5 space-y-5'>
             <p className='text-2xl'>Title: {post.title}</p>
             <p>Body: {post.body}</p>
+            <button className='btn btn-primary hover:btn-secondary' onClick={handleGoBack}>Go Back</button>
         </div>
     );
 };
--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import Header from '../Header/Header';
-import { Outlet } from 'react-router';
+import { Outlet, useNavigation } from 'react-router';
 import Footer from '../Footer/Footer';
 import Sidebar from '../Sidebar/Sidebar';
 
 const Root = () => {
+
+    const navigation = useNavigation();
+    const isNavigating = Boolean(navigation.location);
+
     return (
         <div className='flex flex-col justify-center items-center-safe'>
             <Header></Header>
-            <div className="flex gap-5 justify-evenly items-center">
+            <div className="grid grid-cols-2 gap-5">
                 <Sidebar></Sidebar>
+                    {isNavigating && <span className="loading loading-ball loading-xl">Loading...</span>}
                 <Outlet></Outlet>
             </div>
             <Footer></Footer>
